1815f6064f6c954d1bd630b7226ae27f801f83a3
Paul Bricman <paulbricman@protonmail.com>
2022-02-02 11:49:11 +0100

    feat: connect attention with frontend

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import streamlit as st
+from util import attend, render_html, load
 
 
 st.set_page_config(
-    page_title='👁️ semantic salience')
+    page_title='👁️ semantic salience',
+    layout='wide')
+
+tokenizer, model = load('bert-base-cased')
 
 st.info(
     'ℹ️ This tool is part of [a suite of experimental tools for thought](https://paulbricman.com/thoughtware) which incorporate AI primitives in knowledge work.')
 
 hide_streamlit_style = '''
             <style>
-            #MainMenu {visibility: hidden;}
+            # MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             '''
@@ -19,25 +23,20 @@ st.title('👁️ semantic salience')
 st.markdown('A fusion of artificial and organic attention.')
 st.markdown('---')
 
-query = st.text_input(
+cols = st.columns([1, 2.5])
+query = cols[0].text_input(
     'driving query', help='Specify the overarching query which will drive the salience map.')
-with st.expander('salience map settings', expanded=False):
-    pulsing = st.checkbox('pulsing effect', value=True,
-                          help='Specify whether the salience map should be pulsing.')
-    duration = None
-    if pulsing:
-        duration = st.slider('pulse duration (seconds)', 0., 5., step=0.1, value=1.,
-                             help='Specify how long the pulse should take')
-    focus = st.slider('focus strength', 0., 1., step=0.1, value=0.8,
-                      help='Specify how sharp the focus of the salience map should be. Low focus means the salience is distributed more broadly across tokens. High focus means only a handful of tokens will be attended to. `softmax_temperature = 1 - focus`')
-    color = st.color_picker(
-        'halo color', help='Specify the color of the halo around tokens being attended to.')
+duration = cols[0].slider('pulse duration (seconds)', 0., 5., step=0.1, value=1.,
+                          help='Specify how long the pulse should take')
+focus = cols[0].slider('focus strength', 0., 1., step=0.01, value=0.95,
+                       help='Specify how sharp the focus of the salience map should be. Low focus means the salience is distributed more broadly across tokens. High focus means only a handful of tokens will be attended to. `softmax_temperature = 1 - focus`')
+color = cols[0].color_picker(
+    'halo color', help='Specify the color of the halo around tokens being attended to.', value='#EA2121')
 
-with st.expander('text settings', expanded=False):
-    font_family = st.selectbox(
-        'font family', sorted(['Monospace', 'Times New Roman', 'Arial', 'Helvetica', 'Courier', 'Calibri', 'Georgia', 'Space Grotesk']))
-    font_size = st.slider('font size', 10, 20, step=1, value=14,
-                          help='Specify how big the text should be.')
+font_family = cols[0].selectbox(
+    'font family', sorted(['Monospace', 'Times New Roman', 'Arial', 'Helvetica', 'Courier', 'Calibri', 'Georgia', 'Space Grotesk']))
+font_size = cols[0].slider('font size', 10, 20, step=1, value=14,
+                           help='Specify how big the text should be.')
 
 style = f'''
 <style>
@@ -46,12 +45,11 @@ container {{
     font-family: {font_family}; 
     text-align: justify; }}
 
-.glow {{
-  color: black;
-  animation: glow {duration}s ease-in-out infinite alternate;
+.glow-large {{
+  animation: glow-large {duration}s ease-out infinite alternate;
 }}
 
-@-webkit-keyframes glow {{
+@-webkit-keyframes glow-large {{
   from {{
     text-shadow: 0 0 10px #fff;
   }}
@@ -60,17 +58,52 @@ container {{
     text-shadow: 0 0 10px {color}, 0 0 20px {color}, 0 0 30px {color}, 0 0 40px {color}, 0 0 50px {color};
   }}
 }}
+
+.glow-medium {{
+  animation: glow-medium {duration}s ease-out infinite alternate;
+}}
+
+@-webkit-keyframes glow-medium {{
+  from {{
+    text-shadow: 0 0 10px #fff;
+  }}
+  
+  to {{
+    text-shadow: 0 0 10px {color}, 0 0 20px {color};
+  }}
+}}
+
+.glow-small {{
+  animation: glow-small {duration}s ease-out infinite alternate;
+}}
+
+@-webkit-keyframes glow-small {{
+  from {{
+    text-shadow: 0 0 10px #fff;
+  }}
+  
+  to {{
+    text-shadow: 0 0 10px {color};
+  }}
+}}
 </style>'''
 
 if 'content' not in st.session_state.keys() or st.session_state['content'] == None:
-    content = st.text_area('content', height=300)
-    if st.button('save'):
+    content = cols[1].text_area('content', height=300)
+    if cols[1].button('save'):
         st.session_state['content'] = content
         st.experimental_rerun()
 else:
-    if st.button('reset'):
+    if ('query' not in st.session_state.keys() or query != st.session_state['query']) or \
+            ('focus' not in st.session_state.keys() or focus != st.session_state['focus']):
+        corpus_tokens, attention = attend(
+            st.session_state['content'], query, model, tokenizer)
+        content = render_html(corpus_tokens, attention, focus)
+
+    if cols[0].button('reset content'):
         st.session_state['content'] = None
         st.experimental_rerun()
-    content = style + '<container>' + ''.join(['<p>' + e +
-                                               '</p>' for e in st.session_state['content'].split('\n')]) + '</container>'
-    st.components.v1.html(content, scrolling=True, height=5000)
+
+    content = style + '<container><p>' + content + '</p></container>'
+    with cols[1]:
+        st.components.v1.html(content, scrolling=True, height=5000)
--- util.py
+++ util.py
@@ -1,15 +1,14 @@
-from transformers import AutoTokenizer, AutoModel
+import dis
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-model = 'distilbert-base-cased'
-tokenizer = AutoTokenizer.from_pretrained(model)
-model = AutoModel.from_pretrained(model, output_attentions=True)
+from transformers import AutoTokenizer, AutoModel
+import streamlit as st
+import re
 
 
 def attend(corpus, query, model, tokenizer, blacklist=False):
-    token_blacklist = [119, 136, 106]  # [1012, 1029, 999]
+    token_blacklist = [119, 136, 106]
+    query = '\n\n---\n\n' + query
     full_ids = tokenizer(corpus + '\n\n' + query,
                          return_tensors='pt')['input_ids']
     query_ids = tokenizer(query,
@@ -17,7 +16,7 @@ def attend(corpus, query, model, tokenizer, blacklist=False):
     corpus_ids = tokenizer(corpus + '\n\n',
                            return_tensors='pt')['input_ids']
 
-    attention = [e.detach().numpy() for e in model(full_ids)[-1]][-2]
+    attention = [e.detach().numpy() for e in model(full_ids)[-1]][-1]
     attention = np.array([e[1:-1]
                          for e in np.mean(attention, axis=(0, 1))[1:-1]])
 
@@ -32,8 +31,9 @@ def attend(corpus, query, model, tokenizer, blacklist=False):
                  for e in attention[-(len(query_ids[0]) - 2):]]
 
     attention = np.mean(attention, axis=0)
-    corpus_tokens = tokenizer.convert_ids_to_tokens(corpus_ids[0])[1:-1]
-    plot_attention(attention, corpus_tokens)
+    corpus_tokens = tokenizer.convert_ids_to_tokens(
+        corpus_ids[0], skip_special_tokens=True)
+    # plot_attention(attention, corpus_tokens)
     return corpus_tokens, attention
 
 
@@ -54,5 +54,40 @@ def softmax(x, temperature):
     return e_x / e_x.sum()
 
 
-attend('Anna lived in Constantinople. Omeir lived close to it.',
-       'Where did the two live?', model, tokenizer)
+def render_html(corpus_tokens, attention, focus=0.99):
+    # focus = focus * 0.5 + 0.5
+    mu = np.median(attention)
+    sigma = np.std(attention)
+
+    raw = ''
+
+    distribution = [0, 0, 0]
+    for e_idx, e in enumerate(corpus_tokens):
+        if attention[e_idx] > mu + focus * sigma * 8:
+            distribution[2] += 1
+            raw += ' <span class="glow-large">' + e + '</span>'
+        elif attention[e_idx] > mu + focus * sigma * 2:
+            distribution[1] += 1
+            raw += ' <span class="glow-medium">' + e + '</span>'
+        elif attention[e_idx] > mu + focus * sigma * 1:
+            distribution[0] += 1
+            raw += ' <span class="glow-small">' + e + '</span>'
+        else:
+            raw += ' ' + e
+
+    print(distribution)
+    raw = re.sub(r'\s##', '', raw)
+    raw = re.sub(r'\s(\.|,|!|\?|;|\))', r'\1', raw)
+    raw = re.sub(r'\(\s', r'(', raw)
+    raw = re.sub(r'\s(-|\'|’)\s', r'\1', raw)
+    raw = re.sub(r'\s<span class="glow-(small|medium|large)">##',
+                 r'<span class="glow-\1">', raw)
+    raw = raw.strip()
+    return raw
+
+
+@ st.cache(allow_output_mutation=True)
+def load(model='distilbert-base-cased'):
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    model = AutoModel.from_pretrained(model, output_attentions=True)
+    return tokenizer, model
